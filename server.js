3dd88465f44d7dbc655f2f8bcd4a75950f218610
Antonio Angelino <antonio@type12.com>
2018-09-27 20:48:42 +0200

    Changed port
    Server will start on port 9000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 
 // API calls
 app.get('/api/hello', (req, res) => {
